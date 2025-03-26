3bf2432f4b6465ee30cf6a89dbe0cc248d5d4cf2
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-26 12:01:38 -0300

    Adivinha jogo

---
+++ ProjetoEnquete/src/ExemploRandom.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class ExemploRandom {
+
+    public static void main(String[] args)  {
+
+
+        Random rand = new Random();
+        int numero = rand.nextInt(1000);
+        System.out.println(numero);
+
+
+
+    }
+
+
+
+
+}
